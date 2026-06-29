f782339d8c502702cd16a3c2605e4416fdb1e4ee
moore6js <moore6js@mail.uc.edu>
2026-06-29 10:52:20 -0400

    commit

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -4,33 +4,50 @@ import { removeItem, updateQuantity } from './CartSlice';
 import './CartItem.css';
 
 const CartItem = ({ onContinueShopping }) => {
-  const cart = useSelector(state => state.cart.items);
-  const dispatch = useDispatch();
+    const cart = useSelector(state => state.cart.items);
+    const dispatch = useDispatch();
 
   // Calculate total amount for all products in the cart
-  const calculateTotalAmount = () => {
- 
+    const calculateTotalAmount = () => {
+        let total = 0;
+        cart.forEach(item => {
+        //convert the cost from string to float, multiply it by the quantity and add it to the total
+        total = total + (item.quantity * parseFloat(item.cost.substring(1)))
+        });
+        return total;
+    };
+
+    const handleCheckoutShopping = (e) => {
+        alert('Functionality to be added for future reference');
+    };
+
+
+
+
+    const handleIncrement = (item) => {
+        dispatch(updateQuantity({ name: item.name, quantity: item.quantity + 1 }));
+    };
+
+    const handleDecrement = (item) => {
+        if(item.quantity > 1){
+            dispatch(updateQuantity({ name: item.name, quantity: item.quantity - 1 }));
+        }else{
+            dispatch(removeItem({name: item.name}));
+        }
   };
 
-  const handleContinueShopping = (e) => {
-   
-  };
-
-
-
-  const handleIncrement = (item) => {
-  };
-
-  const handleDecrement = (item) => {
-   
-  };
-
-  const handleRemove = (item) => {
-  };
+    const handleRemove = (item) => {
+        dispatch(removeItem({name: item.name}));dispatch(removeItem({name: item.name}));
+    };
 
   // Calculate total cost based on quantity for an item
-  const calculateTotalCost = (item) => {
-  };
+    const calculateTotalCost = (item) => {
+        let total = 0;
+        cart.forEach(item => {
+            total = total + (item.quantity * parseFloat(item.cost.substring(1)));
+        });
+        return total;
+    };
 
   return (
     <div className="cart-container">
